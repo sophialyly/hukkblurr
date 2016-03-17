84656ac401a44d438bdc787a722c28e820decbb4
hackinhukk <trevorthomas@mac.com>
2016-03-17 10:34:33 -0400

    got the html public pages working properly

--- hukkblurr.go
+++ hukkblurr.go
@@ -2,12 +2,28 @@ package main
 
 import (
   "net/http"
+  "io/ioutil"
 )
 
 func main() {
-  http.HandleFunc("/", func (w http.ResponseWriter, req *http.Request) {
-    w.Write([]byte("Captainhukk Vs Ablurr: Esports Science Investigates"))
-  })
+    http.Handle("/", new(MyHandler))
 
-  http.ListenAndServe(":8000", nil)
+    http.ListenAndServe(":8000", nil)
+}
+
+type MyHandler struct {
+    http.Handler
+}
+
+func (this *MyHandler) ServeHTTP (w http.ResponseWriter, req *http.Request) {
+    path := "public/" + req.URL.Path
+    data, err := ioutil.ReadFile(string(path))
+
+    if err == nil {
+      w.Write(data)
+    } else {
+      w.WriteHeader(404)
+      w.Write([]byte("404 -" + http.StatusText(404)))
+    }
+
 }
